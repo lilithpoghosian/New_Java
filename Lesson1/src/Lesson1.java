a58c77aa803ed943774e17f26d400d862611fbe3
Lilith Poghosian <lilith.poghosian@gmail.com>
2022-08-19 16:02:21 +0400

    My first Commit

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,51 @@
+
+    public class Lesson1 {
+        public static void main(String[] args) {
+            System.out.println("Barev");
+            System.out.println("Lesson 1");
+
+            System.out.println("Java for Automation");
+
+            // a random irrational number is int for JAVA
+
+            byte x= 120;
+            System.out.println(x);
+            short A;
+            System.out.println(A =x);
+
+            long L;
+            System.out.println(L=5555666655588L+66L);
+
+            //a random rational number is double for JAVA
+            float AA;
+            System.out.println(AA=4.5f);
+
+            double BB;
+            System.out.println(BB=44.5);
+
+            //Boolean type can contain only true or false
+
+            boolean AAA = true;
+            boolean BBB = false;
+            System.out.println(AAA);
+            System.out.println(BBB);
+
+            char SS= '?';
+            System.out.println(SS);
+
+            char TT='\u003d';
+            System.out.println(TT);
+
+            System.out.println("True ="+TT);
+
+            String Text ;
+            char CH = '!';
+            System.out.println(Text = "Hello Java, I'm a beginner" + CH );
+
+
+
+
+        }
+    }
+
+
